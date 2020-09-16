5e338ee02a35ed1898dc6153fded728c2ca17c56
LeandroLuna <leandro.j.luna@gmail.com>
2020-09-16 17:48:31 -0300

    16/09

---
+++ modulo MFRC522/escrita.py
@@ -0,0 +1,15 @@
+# todas funçoes necessarias para interagir com os  PINS
+import RPi.GPIO as GPIO
+# funções do modulo simplificadas, também possivel usar a biblioteca MFRC522
+from mfrc522 import SimpleMFRC522
+
+# cria uma copia da biblioteca SimpleMFRC522 como um objeto. É a onde ira armazenar todas as configuraçoes escritas na TAG.
+leia = SimpleMFRC522()
+
+try:
+    texto = input('Dado a ser escrito na TAG: ')
+    print("Posicione sua TAG para escrita")
+    leia.write(texto)
+    print("Escrita!")
+finally:
+    GPIO.cleanup()  # Limpar saida para não confundir demais scripts
---
+++ modulo MFRC522/leitura.py
@@ -0,0 +1,16 @@
+# todas funçoes necessarias para interagir com os  PINS
+import RPi.GPIO as GPIO
+# funções do modulo simplificadas, também possivel usar a biblioteca MFRC522
+from mfrc522 import SimpleMFRC522
+
+# cria uma copia da biblioteca SimpleMFRC522 como um objeto. É a onde ira armazenar todas as configuraçoes escritas na TAG que chamare-mos posteriomente.
+reader = SimpleMFRC522()
+
+try:
+    # Chama o nosso objeto de leitura; envia para o circuito uma ordem para começar uma leitura de qualquer TAG posicionado sobre o leitor RC522.
+    id, texto = leia.read()
+    print(id)
+    print(texto)
+finally:
+    # Limpar saida para não confundir demais scripts
+    GPIO.cleanup()
