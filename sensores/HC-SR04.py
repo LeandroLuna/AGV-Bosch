e0d2f2914e75a33cda9fa65b76cbc548db242591
LeandroLuna <leandro.j.luna@gmail.com>
2020-09-20 20:21:56 -0300

    20/09

--- sensores/HC-SR04.py
+++ sensores/HC-SR04.py
@@ -2,36 +2,39 @@ import RPi.GPIO as GPIO
 import time
 
 try:
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)  # Para numerar os pinos fisicos
 
-    PIN_TRIGGER = 7
-    PIN_ECHO = 11
+    PIN_TRIGGER = 7  # Define o pino do sensor que emitira a onda sonora
+    PIN_ECHO = 11  # Define o pino do sensor que receberá a onda sonora
 
-    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-    GPIO.setup(PIN_ECHO, GPIO.IN)
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)  # Enviar a onda, OUT
+    GPIO.setup(PIN_ECHO, GPIO.IN)  # Receber a onda, IN
 
+    # Garante que o sensor estará desligado, para maior precisão do mensuramento da distancia do objeto
     GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    print("Esperando o sensor estabilizar")
+    time.sleep(2)  # Tempo para o mesmo estabilizar
 
-    print("Waiting for sensor to settle")
+    # Começo do processo para calcular a distancia, aviso para o usuario
+    print("Calculando distância")
 
-    time.sleep(2)
-
-    print("Calculating distance")
-
+    # Liga o sensor que enviara a onda sonora
     GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-    time.sleep(0.00001)
+    time.sleep(0.00001)  # O sensor requer um de 1 nanosegundo para ativar
 
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)  # Desligamos novamente
 
-    while GPIO.input(PIN_ECHO) == 0:
-        pulse_start_time = time.time()
+    while GPIO.input(PIN_ECHO) == 0:  # Checaremos se o receptor da onda está desligado (não recebeu o sinal). Caso não, enviaremos para a variavel o tempo atual até que a condição se torne falsa (ou seja, 1, HIGH)
+        tempo_inicial = time.time()  # Tempo de saida da onda
+    # Checaremos se o receptor da onda está ligado (recebeu o sinal). Caso sim, gravará em uma variavel o tempo atual até o pin ECHO estiver desligado novamente.
     while GPIO.input(PIN_ECHO) == 1:
-        pulse_end_time = time.time()
+        tempo_final = time.time()  # Tempo de chegada da onda
 
-    pulse_duration = pulse_end_time - pulse_start_time
-    distance = round(pulse_duration * 17150, 2)
-    print("Distance:", distance, "cm")
+    # Calcula a diferença do tempo de chegada e envio da onda sonora
+    duracao_onda = tempo_final - tempo_inicial
+    distancia = round(duracao_onda * 17150, 2)  # Calcula a distancia da onda, em centimetros, e arredonda a mesma para printar ao usuario. A velocidade ultrassonica do som: 34300 cm/s. Como so queremos a distancia percorrida até o objeto, devemos dividir esse valor por 2, dando o valor de 17.150cm/s, no qual devemos multiplicar pela duraçao do impulso da onda (diferença entre saida e chegada) para obtermos o resultado da distancia em centimetros
+    print(f"Distancia: {distancia}cm")
 
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup()  # Limpa as portas GPIO para não entrar em conflito com nenhum sensor
--- sensores/QRE1113.py
+++ sensores/QRE1113.py
@@ -7,7 +7,8 @@ gpio.setmode(gpio.BOARD)
 # definir os pinos GPIO como output/'saida'
 gpio.setup(13, gpio.OUT)
 gpio.setup(15, gpio.OUT)
-
+gpio.setup(19, gpio.OUT)
+gpio.setup(21, gpio.OUT)
 
 # definir os pinos GPIO como input/'entrada'
 leftSensor = 7
@@ -16,28 +17,44 @@ gpio.setup(leftSensor, gpio.IN)
 gpio.setup(rightSensor, gpio.IN)
 
 
-def leftOn():  # ligar motor esquerdo
+def leftFOn():  # ligar motor esquerdo frontal
     gpio.output(15, 1)
 
 
-def leftOff():  # desligar motor esquerdo
+def leftFOff():  # desligar motor esquerdo frontal
     gpio.output(15, 0)
 
 
-# ligar motor direito
-def rightOn():
+def leftTOn():  # ligar motor esquerdo traseiro
+    gpio.output(19, 1)
+
+
+def leftTOff():  # desligar motor esquerdo traseiro
+    gpio.output(19, 0)
+
+
+def rightFOn():  # ligar motor direito frontal
     gpio.output(13, 1)
 
 
-# desligar motor direito
-def rightOff():
+def rightFOff():  # desligar motor direito frontal
     gpio.output(13, 0)
+
+
+def rightTOn():  # ligar motor direito traseiro
+    gpio.output(21, 1)
+
+
+def rightTOff():  # desligar motor direito traseiro
+    gpio.output(21, 0)
 
 
 # desligar todos os motores
 def stopAll():
     gpio.output(13, 0)
     gpio.output(15, 0)
+    gpio.output(19, 0)
+    gpio.output(21, 0)
 
 
 # chamar a função para confirmar que os motores estão desligados
@@ -46,25 +63,32 @@ stopAll()
 gpio.setwarnings(False)
 
 while True:  # Loop principal
-    # se ambos os sensores estiverem desligados, desligar ambos os motores
+    # se ambos os sensores estiverem desligados, desligar todos os motores
     if gpio.input(leftSensor) == 0 and gpio.input(rightSensor) == 0:
-        leftOff()
-        rightOff()
+        leftFOff()
+        rightFOff()
+        leftTOff()
+        rightTOff()
 
     # se ambos os sensores estiverem ligados, ligar ambos os motores
     if gpio.input(leftSensor) == 1 and gpio.input(rightSensor) == 1:
-        leftOn()
-        rightOn()
+        leftFOn()
+        rightFOn()
+        leftTOn()
+        rightTOn()
 
     # se o sensor da esquerda estiver ligado, desligar motor da direita (virar esquerda)
     if gpio.input(leftSensor) == 1 and gpio.input(rightSensor) == 0:
-        leftOn()
-        rightOff()
-
+        leftFOn()
+        rightFOff()
+        leftTOn()
+        rightTOff()
     # se o sensor da direita estiver ligado, desligar motor da esquerda (virar direita)
     if gpio.input(leftSensor) == 0 and gpio.input(rightSensor) == 1:
-        leftOff()
-        rightOn()
+        leftFOff()
+        rightFOn()
+        leftTOff()
+        rightTOn()
 
 # limpar os pinos para recomeçar o loop
 gpio.cleanup()
