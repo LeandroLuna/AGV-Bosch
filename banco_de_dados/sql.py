858b9ba342e6fa985346a8cb4c68f16cfcc74d8b
LeandroLuna <leandro.j.luna@gmail.com>
2020-09-26 22:11:49 -0300

    26/09

--- banco_de_dados/sql.py
+++ banco_de_dados/sql.py
@@ -6,10 +6,15 @@ Local = input('Informe a localizaçao do produto: ')
 
 conexao = sqlite3.connect('estoques.db')
 cursor = conexao.cursor()
-cursor.execute('create table estoques(ID text, Localização text)')
+#cursor.execute('create table estoques(ID integer, Local text)')
 cursor.execute(
-    'insert into estoques(ID, Localização) values (?,?)', (ID, Local))
+    'insert into estoques(ID, Local) values (?,?)', (ID, Local))
 conexao.commit()
+cursor.execute('SELECT * FROM estoques')
+estoque_apresentar = cursor.fetchall()
+
+for banco_dados in estoque_apresentar:
+    print('ID: %s \nLocalizaçao: %s' % (estoque_apresentar))
 
 cursor.close()
 conexao.close()
